e44503bee534585414e130cc612df812a1b214f0
eweymuller <eweymuller@gmail.com>
2022-03-19 14:44:27 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Eva Weymuller and is open source code on{" "}
-        <a
-          href="https://github.com/eweymuller/react-scw4"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Eva Weymuller and is open source code on{" "}
+          <a
+            href="https://github.com/eweymuller/react-scw4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
