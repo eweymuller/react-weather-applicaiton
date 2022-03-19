e2ea69c0566e2a359da3de88645d69c5a3d1b740
eweymuller <eweymuller@gmail.com>
2022-03-19 14:19:03 -0700

    netlify failed to deploy in building phase: RETRY

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1> Weather App</h1>
       <footer>
         This project was coded by Eva Weymuller and is open source code on{" "}
         <a href="https://github.com/eweymuller/react-scw4" target="_blank">
