6e3ac979a1d6a4e0ea9eeba341b2265616041360
eweymuller <eweymuller@gmail.com>
2022-03-19 14:16:56 -0700

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Eva Weymuller and is open source code on{" "}
+        <a href="https://github.com/eweymuller/react-scw4" target="_blank">
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
