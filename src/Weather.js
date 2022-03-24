ea3372e22ad12b195ea6356e7c8a9fe0c99eda67
eweymuller <eweymuller@gmail.com>
2022-03-24 10:05:59 -0700

    reverting back fixing classNameName problem

--- src/Weather.js
+++ src/Weather.js
@@ -1,105 +1,89 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  const [weatherData, setWeatherData] = useState({ ready: false });
-  function handleResponse(response) {      
-    console.log(response.data)
-    setWeatherData({
-      ready: true,
-})
-  if (weatherData.ready) {
-    return (
-      <div classNamName="Weather">
-        <form>
-          <div className="row">
-            <div className="col-10">
-              <input
-                type="search"
-                placeholder="Enter a city..."
-                className="search-bar"
-              />
-              <input type="submit" value="search" className="button-default" />
-            </div>
-            <div className="col-2">
-              <button className="unitConversion button-default">
-                Imperial/metric
-              </button>
-            </div>
-          </div>
-        </form>
-
-        <div className="row col-6">
-          <h1 className="city">Chamonix</h1>
-          <p className="country">France</p>
-          <hr />
-        </div>
+  return (
+    <div classNamName="Weather">
+      <form>
         <div className="row">
-          <div className="col-7">
-            <div className="row">
-              <div className="col-3 dark">Today</div>
-              <div className="col-3 dark">High</div>
-              <div className="col-3 dark">Low</div>
-            </div>
-            <div className="row">
-              <div className="col-3 light">--/--</div>
-              <div className="col-5"></div>
-              <div className="col-2">
-                <img
-                  className="icon"
-                  src="https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png"
-                  alt="Weather Icon"
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-3"></div>
-              <div className="col-3 light">
-                <h6 className="high-temp">
-                  <span>--</span>°C
-                </h6>
-              </div>
-              <div className="col-3 light">
-                <h6 className="low-temp">
-                  <span>--</span>°C
-                </h6>
-              </div>
-            </div>
+          <div className="col-10">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="search-bar"
+            />
+            <input type="submit" value="search" className="button-default" />
           </div>
           <div className="col-2">
-            <div className="row dark">Wind</div>
-            <div className="row dark">Humidity</div>
-            <div className="row dark">Feels Like</div>
+            <button className="unitConversion button-default">
+              Imperial/metric
+            </button>
           </div>
-          <div className="col-1"></div>
-          <div className="col-2">
-            <div className="row light">
-              <h6 className="wind-value">
-                <span>--</span>kph
+        </div>
+      </form>
+
+      <div className="row col-6">
+        <h1 className="city">Chamonix</h1>
+        <p className="country">France</p>
+        <hr />
+      </div>
+      <div className="row">
+        <div className="col-7">
+          <div className="row">
+            <div className="col-3 dark">Today</div>
+            <div className="col-3 dark">High</div>
+            <div className="col-3 dark">Low</div>
+          </div>
+          <div className="row">
+            <div className="col-3 light">--/--</div>
+            <div className="col-5"></div>
+            <div className="col-2">
+              <img
+                className="icon"
+                src="https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png"
+                alt="Weather Icon"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-3"></div>
+            <div className="col-3 light">
+              <h6 className="high-temp">
+                <span>--</span>°C
               </h6>
             </div>
-            <div className="row light">
-              <h6 className="humidity-value">
-                <span>--</span>%
-              </h6>
-            </div>
-            <div className="row light">
-              <h6 className="feel-value">
+            <div className="col-3 light">
+              <h6 className="low-temp">
                 <span>--</span>°C
               </h6>
             </div>
           </div>
         </div>
-        <div className="card forecast"></div>
+        <div className="col-2">
+          <div className="row dark">Wind</div>
+          <div className="row dark">Humidity</div>
+          <div className="row dark">Feels Like</div>
+        </div>
+        <div className="col-1"></div>
+        <div className="col-2">
+          <div className="row light">
+            <h6 className="wind-value">
+              <span>--</span>kph
+            </h6>
+          </div>
+          <div className="row light">
+            <h6 className="humidity-value">
+              <span>--</span>%
+            </h6>
+          </div>
+          <div className="row light">
+            <h6 className="feel-value">
+              <span>--</span>°C
+            </h6>
+          </div>
+        </div>
       </div>
-    );
-  } else {
-    const apiKey = "7633347349ec94a368e4a15d93744b30";
-    let city = "chamonix";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
-    return "loading...";
-  }
-  }
+      <div className="card forecast"></div>
+    </div>
+  );
+}
