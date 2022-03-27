e750a7b8bebd4a530f86009f04a896a1c44945ce
eweymuller <eweymuller@gmail.com>
2022-03-27 14:15:40 -0700

    Moving Weather to new component WeatherInfo

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
 export default function Weather() {
@@ -34,78 +34,13 @@ export default function Weather() {
               <input type="submit" value="search" className="button-default" />
             </div>
             <div className="col-2">
-              <button className="unitConversion button-default">
-                Imperial/metric
-              </button>
+              <button className="button-default">Imperial/metric</button>
             </div>
           </div>
         </form>
 
-        <div className="row col-6">
-          <h1 className="city">Chamonix</h1>
-          <p className="country dark">{weatherData.country}</p>
-          <hr />
-        </div>
-        <div className="row">
-          <div className="col-7">
-            <div className="row">
-              <div className="col-3 dark title">Today</div>
-              <div className="col-3 dark title">High</div>
-              <div className="col-3 dark title">Low</div>
-            </div>
-            <div className="row">
-              <div className="col-3 light">
-                <h6>
-                  <FormattedDate date={weatherData.date} />
-                </h6>
-              </div>
-              <div className="col-5"></div>
-              <div className="col-2">
-                <img
-                  className="icon"
-                  src="http://openweathermap.org/img/wn/03d@2x.png"
-                  alt="Weather Icon"
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-3"></div>
-              <div className="col-3 light">
-                <h6>
-                  <span>{Math.round(weatherData.tempHigh)}</span>°C
-                </h6>
-              </div>
-              <div className="col-3 light">
-                <h6>
-                  <span>{Math.round(weatherData.tempLow)}</span>°C
-                </h6>
-              </div>
-            </div>
-          </div>
-          <div className="col-2">
-            <div className="row dark title">Wind</div>
-            <div className="row dark title">Humidity</div>
-            <div className="row dark title">Feels Like</div>
-          </div>
-          <div className="col-1"></div>
-          <div className="col-2">
-            <div className="row light">
-              <h6>
-                <span>{Math.round(weatherData.wind)}</span>kph
-              </h6>
-            </div>
-            <div className="row light">
-              <h6>
-                <span>{Math.round(weatherData.humidity)}</span>%
-              </h6>
-            </div>
-            <div className="row light">
-              <h6>
-                <span>{Math.round(weatherData.feel)}</span>°C
-              </h6>
-            </div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
+
         <div className="card forecast"></div>
       </div>
     );
@@ -114,6 +49,10 @@ export default function Weather() {
     let city = "chamonix";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-    return "Loading...";
+    return (
+      <div>
+        <h6>Loading...</h6>
+      </div>
+    );
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,75 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import "./Weather.css";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="row col-6">
+        <h1 className="city">Chamonix</h1>
+        <p className="country dark">{props.weatherData.country}</p>
+        <hr />
+      </div>
+      <div className="row">
+        <div className="col-7">
+          <div className="row">
+            <div className="col-3 dark title">Today</div>
+            <div className="col-3 dark title">High</div>
+            <div className="col-3 dark title">Low</div>
+          </div>
+          <div className="row">
+            <div className="col-3 light">
+              <h6>
+                <FormattedDate date={props.weatherData.date} />
+              </h6>
+            </div>
+            <div className="col-5"></div>
+            <div className="col-2">
+              <img
+                className="icon"
+                src="http://openweathermap.org/img/wn/03d@2x.png"
+                alt="Weather Icon"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-3"></div>
+            <div className="col-3 light">
+              <h6>
+                <span>{Math.round(props.weatherData.tempHigh)}</span>°C
+              </h6>
+            </div>
+            <div className="col-3 light">
+              <h6>
+                <span>{Math.round(props.weatherData.tempLow)}</span>°C
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div className="col-2">
+          <div className="row dark title">Wind</div>
+          <div className="row dark title">Humidity</div>
+          <div className="row dark title">Feels Like</div>
+        </div>
+        <div className="col-1"></div>
+        <div className="col-2">
+          <div className="row light">
+            <h6>
+              <span>{Math.round(props.weatherData.wind)}</span>kph
+            </h6>
+          </div>
+          <div className="row light">
+            <h6>
+              <span>{Math.round(props.weatherData.humidity)}</span>%
+            </h6>
+          </div>
+          <div className="row light">
+            <h6>
+              <span>{Math.round(props.weatherData.feel)}</span>°C
+            </h6>
+          </div>
+        </div>
+      </div>{" "}
+    </div>
+  );
+}
