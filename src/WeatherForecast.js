730fb22b0e387a0f8013eb8b5c8e7881d733ab00
eweymuller <eweymuller@gmail.com>
2022-04-10 14:10:12 +0200

    cleaning up code, organization

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,6 +15,14 @@ export default function WeatherForecast(props) {
     setforecast(response.data.daily);
     setLoaded(true);
   }
+  function load() {
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiKey = "4d4b3a75d801ab1da9179263d8ed4edd";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    Axios.get(apiUrl).then(handleResponse);
+  }
 
   if (loaded) {
     return (
@@ -33,13 +41,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let latitude = props.coordinates.lat;
-    let longitude = props.coordinates.lon;
-    let apiKey = "4d4b3a75d801ab1da9179263d8ed4edd";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    Axios.get(apiUrl).then(handleResponse);
-
+    load();
     return null;
   }
 }
